02d451cf23b4b295fa6f7621afdd9b8d89bed2da
claudiocfls <claudiosilva.cfls@gmail.com>
2019-03-07 18:24:21 -0300

    Books in shelf has random colors

--- lab1/makeShelfWithBooks.js
+++ lab1/makeShelfWithBooks.js
@@ -8,6 +8,12 @@ function makeShelfWithBooks() {
         for(var j=0;j<height.length;j++){
             for(var i=0;i<number_of_books;i++){
                 var a = book.clone();
+                var randomColor = Math.floor(Math.random() * 0xFFFFFF);
+                for(var k=0;k<a.children.length;k++){
+                    a.children[k].material = book.children[k].material.clone();
+                    a.children[k].material.color.setHex(randomColor); 
+                }
+                a.children[3].material.color.setHex(0xFFFFFF);
                 a.position.z = z_0*i + 0.03;
                 a.position.y = 3/2;
                 a.position.y = height[j];
