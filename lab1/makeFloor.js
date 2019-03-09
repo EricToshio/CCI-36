f800d28c33d539c7c45247396bfe87cc89bd6537
claudiocfls <claudiosilva.cfls@gmail.com>
2019-03-09 08:34:11 -0300

    Add screenshots

---
+++ lab1/makeFloor.js
@@ -0,0 +1,32 @@
+// piso
+
+function makeFloor() {
+
+    tile_square_geo = new THREE.PlaneGeometry(10,10)
+    tile_square_mat = new THREE.MeshStandardMaterial({color:0x000000})
+    tile_square     = new THREE.Mesh(tile_square_geo, tile_square_mat)
+    tile_square.rotation.x = -Math.PI/2.0
+    tile_square.position.x = -20
+    tile_square.position.z = -30
+
+    var tileGroup = new THREE.Group();
+    for (var i=0;i<=6;i++){
+        for(var j=0;j<=4;j++){
+            if( (i%2 == 0 && j%2 == 0) || (i%2 == 1 && j%2 == 1) ) {
+                var tileAdd = tile_square.clone();
+                tileAdd.position.x = -20 + j*10;
+                tileAdd.position.z = -30 + i*10;
+                tileGroup.add(tileAdd);
+            } else {
+                var tileAdd = tile_square.clone();
+                tileAdd.material = tile_square.material.clone();
+                tileAdd.material.color.set(0xFFFFFF);
+                tileAdd.position.x = -20 + j*10;
+                tileAdd.position.z = -30 + i*10;
+                tileGroup.add(tileAdd);
+            }
+        }
+    }
+
+    return tileGroup;
+}
--- lab1/makeShelf.js
+++ lab1/makeShelf.js
@@ -1,38 +1,38 @@
 function makeShelf() {
 
-        var texture = new THREE.TextureLoader().load("wood.jpg");
-        //vertical
-        var color_shelf = 0x8B4513;
-        var width = 7;
-        var vertical_geom= new THREE.BoxGeometry(5,15,0.1)
-        // var vertical_mat= new THREE.MeshLambertMaterial({color:color_shelf})
-        var vertical_mat= new THREE.MeshLambertMaterial({map:texture})
-        var vertical = new THREE.Mesh(vertical_geom, vertical_mat)
-        vertical.position.x = 0
-        vertical.position.y = 0
+    var texture = new THREE.TextureLoader().load("wood.jpg");
+    //vertical
+    var color_shelf = 0x8B4513;
+    var width = 7;
+    var vertical_geom= new THREE.BoxGeometry(5,15,0.1)
+    // var vertical_mat= new THREE.MeshLambertMaterial({color:color_shelf})
+    var vertical_mat= new THREE.MeshLambertMaterial({map:texture})
+    var vertical = new THREE.Mesh(vertical_geom, vertical_mat)
+    vertical.position.x = 0
+    vertical.position.y = 0
 
-        var vertical2 = vertical.clone();
-        vertical2.position.z = width;
+    var vertical2 = vertical.clone();
+    vertical2.position.z = width;
 
-        //horizontal
-        var horizontal_geom= new THREE.BoxGeometry(5,0.1,width)
-        // var horizontal_mat= new THREE.MeshLambertMaterial({color:color_shelf})
-        var horizontal_mat= new THREE.MeshLambertMaterial({map:texture})
-        var horizontal = new THREE.Mesh(horizontal_geom, horizontal_mat)
-        horizontal.position.x = 0
-        horizontal.position.y = 0
-        horizontal.position.z = width/2
+    //horizontal
+    var horizontal_geom= new THREE.BoxGeometry(5,0.1,width)
+    // var horizontal_mat= new THREE.MeshLambertMaterial({color:color_shelf})
+    var horizontal_mat= new THREE.MeshLambertMaterial({map:texture})
+    var horizontal = new THREE.Mesh(horizontal_geom, horizontal_mat)
+    horizontal.position.x = 0
+    horizontal.position.y = 0
+    horizontal.position.z = width/2
 
-        var group = new THREE.Group();
-
-        var height = [-4.8,0,4.8]
-        for(var i=0;i<height.length;i++){
-            var new_horizontal = horizontal.clone();
-            new_horizontal.position.y = height[i];
-            group.add(new_horizontal);
-        }
-
-        group.add(vertical);
-        group.add(vertical2);
-        return group;
+    var group = new THREE.Group();
+    group.add(horizontal);
+    var height = [-4.8,4.8]
+    for(var i=0;i<height.length;i++){
+        var new_horizontal = horizontal.clone();
+        new_horizontal.position.y = height[i];
+        group.add(new_horizontal);
     }
+
+    group.add(vertical);
+    group.add(vertical2);
+    return group;
+}
