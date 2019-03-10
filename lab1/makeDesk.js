ed4129163ffe5692fe4430b8b53c904393b2bb98
unknown <eric.toshio.e.s@gmail.com>
2019-03-10 01:49:11 -0300

    improve comments in makeDesk()

--- lab1/makeDesk.js
+++ lab1/makeDesk.js
@@ -1,19 +1,16 @@
 function makeDesk() {
 
-    var color_desk = 0x8B4513;
+    // Configurações e variaveis gerais
     var group = new THREE.Group()
-
     var texture = new THREE.TextureLoader().load("wood.jpg");
-    //pes
-
+    
+    // Gerar uma das pernas
     var leg_geom= new THREE.BoxGeometry(0.6,5,0.6)
-    // var leg_mat= new THREE.MeshLambertMaterial({color:color_desk})
     var leg_mat= new THREE.MeshLambertMaterial({map:texture})
     var leg = new THREE.Mesh(leg_geom, leg_mat)
 
+    // Gerar as outras pernas a partir da exitente
     positions=[ [0,0], [7, 0], [7, 4], [0, 4] ]
-
-    
     for (var i=0; i<positions.length; i++)
     {
         x=positions[i][0]
@@ -25,18 +22,15 @@ function makeDesk() {
         group.add(m)
     }
 
-    
-
-    //horizontal
+    // Gerar superficie horizontal da mesa
     var horizontal_geom= new THREE.BoxGeometry(10,0.4,6)
-    // var horizontal_mat= new THREE.MeshLambertMaterial({color:color_desk})
     var horizontal_mat= new THREE.MeshLambertMaterial({map:texture})
     var horizontal = new THREE.Mesh(horizontal_geom, horizontal_mat)
     horizontal.position.x = 7/2
     horizontal.position.y = 5
     horizontal.position.z = 4/2
-
     group.add(horizontal);
 
+    // Retornar mesa feita
     return group;
 }
