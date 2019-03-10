9d9108161e206f63960a3b26ce20061257d01e9a
unknown <eric.toshio.e.s@gmail.com>
2019-03-10 02:03:06 -0300

    improve comments in makeChair()

--- lab1/makeChair.js
+++ lab1/makeChair.js
@@ -1,17 +1,16 @@
 function makeChair()
 {
-	// Caracteristicas
+	// Configurações e variaveis gerais
 	var group = new THREE.Group()
     var texture = new THREE.TextureLoader().load("wood.jpg");
 
-    // pes
+    // Gerar uma das pernas
 	var leg_geom= new THREE.BoxGeometry(0.6,2.5,0.6)
 	var leg_mat= new THREE.MeshLambertMaterial({map:texture})
 	var leg = new THREE.Mesh(leg_geom, leg_mat)
 
+	// Gerar as outras pernas a partir da exitente
 	positions=[ [0,0], [3, 0], [3, 3], [0, 3] ]
-
-
 	for (var i=0; i<positions.length; i++)
 	{
 		x=positions[i][0]
@@ -23,24 +22,20 @@ function makeChair()
 		group.add(m)
 	}
 
-	// acento
-
+	// Gerar acento
 	var seat_geom = new THREE.BoxGeometry(4,0.4,4)
 	var seat_mat= new THREE.MeshLambertMaterial({map:texture})
 	var seat=new THREE.Mesh(seat_geom,seat_mat)
-
 	seat.position.set(3/2,2.5,3/2)
 	group.add(seat)
 
-	// apoio de costas 
+	// Gerar apoio de costas
 	var backrest_geom = new THREE.BoxGeometry(4.2,5.0,0.2)
 	var backrest_mat= new THREE.MeshLambertMaterial({map:texture})
 	var backrest=new THREE.Mesh(backrest_geom,backrest_mat)
-
 	backrest.position.set(3/2,5,-0.3)
-
-
 	group.add(backrest)
 
+	// Retornar cadeia feita
 	return group
 }
